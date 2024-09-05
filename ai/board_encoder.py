bfcac64594c40565564b5929ae7effeeb48f326f
Johannes Wolf <Johannes.norbert.wolf+github@gmail.com>
2024-09-05 14:16:07 +0200

    feat: remove unused expample usage from board encoder

--- ai/board_encoder.py
+++ ai/board_encoder.py
@@ -29,14 +29,3 @@ class BoardEncoder:
             point = board.points[i]
             encoded_board.extend(self.encode_point(point))
         return np.array(encoded_board)
-
-# Example usage
-if __name__ == "__main__":
-    config_loader = ConfigLoader("../config/config.yml")
-    board = GameBoard(config_loader)
-    board.initialize_board()
-
-    encoder = BoardEncoder(config_loader)
-    encoded_board = encoder.encode_board(board, 0)
-    print(encoded_board)
-    print(len(encoded_board))  # Should be (board_size + 2) * (4 + pieces_per_player) bits
